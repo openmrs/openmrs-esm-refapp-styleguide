cfd795902e86740657c110329113392e7012d2d0
MD Dubey <mddubey409@gmail.com>
2020-06-11 15:40:12 +0530

    Remove unwanted load-css.js

--- src/load-css.js
+++
@@ -1,5 +0,0 @@
-const linkElement = document.createElement("link");
-linkElement.rel = "stylesheet";
-linkElement.href =
-  __webpack_public_path__ + "openmrs-esm-refapp-styleguide.css";
-document.head.appendChild(linkElement);
