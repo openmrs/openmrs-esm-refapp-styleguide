1a3d6a19c3b285cb0cbd5b347f2bb3f850d442ac
MD Dubey <mddubey409@gmail.com>
2020-06-11 15:38:37 +0530

    Add jsonPFunction for output

--- src/openmrs-refapp-styleguide.js
+++ src/openmrs-refapp-styleguide.js
@@ -1,13 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 
-import "./load-css.js";
+// import "./load-css.js";
 import "@openmrs/style-referenceapplication/lib/referenceapplication.css";
 import "./variables.css";
 import "./custom.css";
 import "./refapp-patient-chart.css"
 
-const lifecycles = singleSpaReact();
+function Root(props){
+    return "";
+}
+
+const { bootstrap, mount, unmount } = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: Root
+  });
   
-export const bootstrap = lifecycles.bootstrap;
-export const mount = lifecycles.mount;
-export const unmount = lifecycles.unmount;
+export { bootstrap, mount, unmount };
